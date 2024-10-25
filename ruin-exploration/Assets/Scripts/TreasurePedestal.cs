c15ff0db98ea508aabd1b00b0724314c0bc197b7
Jyl7hf <lijillian@hotmail.com>
2024-10-25 16:08:45 -0700

    treasure pedestal raises

--- ruin-exploration/Assets/Scripts/TreasurePedestal.cs
+++ ruin-exploration/Assets/Scripts/TreasurePedestal.cs
@@ -6,18 +6,28 @@ public class TreasurePedestal : MonoBehaviour
 {
     [SerializeField]
     ObjectPedestal[] objectPedestals;
+    [SerializeField]
+    Transform target;
+    
+    bool revealTreasure;
+    float moveSpeed = 3;
 
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-        
-    // }
+    // Start is called before the first frame update
+    void Start()
+    {
+        revealTreasure = false;
+        Debug.Log(target.position);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(allPedestalsActivated()) {
+        if(!revealTreasure && allPedestalsActivated()) {
             Debug.Log("all objects correct");
+            revealTreasure = true;
+        }
+        else if(revealTreasure) {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
         }
     }
 
