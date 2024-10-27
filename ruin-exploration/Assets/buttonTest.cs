5265b40632968546a5b3a02f4a2be198fe6fde82
kuihua <michellelin2235@gmail.com>
2024-10-27 06:18:15 -0700

    button

---
+++ ruin-exploration/Assets/buttonTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonTest : MonoBehaviour
+{
+    public bool appear = true;
+    [SerializeField] public GameObject gb;
+    public void ChangeState() {
+        if(appear) {
+            gb.SetActive(false);
+            appear = false;
+        } else {
+            gb.SetActive(true);
+            appear = true;
+        }
+    }
+}
